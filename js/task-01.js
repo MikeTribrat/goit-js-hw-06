5150c8bd90f56a045d2dd1eafaed17ef691c5349
MikeTribrat <mike.lapp.ua@gmail.com>
2022-04-19 17:40:50 +0300

    Update task-01.js
    1st done

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,7 @@ const categoryList = document.querySelector('#categories').children;
 console.log(`Number of categories: ${categoryList.length}`);
 
 const categoryListEl = document.querySelectorAll('.item')
-    .forEach((el) =>
-        console.log('Category:', el.querySelector('h2').textContent),
-        console.log('Elements:', document.querySelector('li').length));
+    .forEach((el) => {
+        console.log('Category:', el.querySelector('h2').textContent);
+        console.log('Elements:', el.querySelectorAll('li').length);
+    })
