817110454e78be025ffccee6e979ebe2f7c09404
MikeTribrat <mike.lapp.ua@gmail.com>
2022-04-19 18:51:11 +0300

    Update task-02.js
    2nd done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredients[i];
+  listEl.classList.add('item');
+  console.log(listEl);
+  const navEl = document.querySelector('#ingredients');
+  navEl.appendChild(listEl);
+}
