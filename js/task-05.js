cb2fe3513797e54dd38ffe55ca303b59cd7721fc
MikeTribrat <mike.lapp.ua@gmail.com>
2022-07-16 11:50:38 +0300

    hw6 done

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+
+const inputRef = document.querySelector("#name-input");
+const outInput = document.querySelector("#name-output");
+const h1El = document.querySelector("h1");
+
+const onInputChange = event =>
+    inputRef.value === "" ? outInput.textContent = "Anonymous" : outInput.textContent = event.target.value;
+
+inputRef.addEventListener("input", onInputChange);
